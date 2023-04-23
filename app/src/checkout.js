e5cae54a9945a4547cb4a68a25c108db6e7b01bd
hwangahn <114152522+hwangahn@users.noreply.github.com>
2023-04-24 00:30:51 +0700

    Update checkout.js

--- app/src/checkout.js
+++ app/src/checkout.js
@@ -1,9 +1,11 @@
-import { Result, Select, Space } from "antd";
+import { Button, Result, Select, Space, message } from "antd";
 import { useEffect, useState } from "react";
 
 function OrderForm() {
 
+    let [city, setCity] = useState();
     let [district, setDistrict] = useState();
+    let [ward, setWard] = useState();
     let [cityList, setCityList] = useState(null);
     let [districtList, setDistrictList] = useState(null);
     let [wardList, setWardList] = useState(null);
@@ -24,6 +26,9 @@ function OrderForm() {
 
     let getDistrictList = (value) => {
         setExpectedArrival("");
+        setCity(value);
+        setDistrict();
+        setWard();
         setWardList(null);
         setDistrictList(null);
         fetch('https://online-gateway.ghn.vn/shiip/public-api/master-data/district', {
@@ -43,6 +48,7 @@ function OrderForm() {
     let getWardList = (value) => {
         setExpectedArrival("");
         setDistrict(value);
+        setWard();
         setWardList(null);
         fetch('https://online-gateway.ghn.vn/shiip/public-api/master-data/ward', {
             method: 'post',
@@ -59,6 +65,7 @@ function OrderForm() {
     }
 
     let getExpArr = (value) => {
+        setWard(value);
         fetch('https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/leadtime', {
             method: 'post',
             headers: {
@@ -70,7 +77,7 @@ function OrderForm() {
                 from_district_id: 1493,
                 from_ward_code: "1A0710",
                 to_district_id: district,
-                to_ward_code: value,
+                to_ward_code: ward,
                 service_id: 53320
             })
         })
@@ -81,32 +88,41 @@ function OrderForm() {
         });
     }
 
+    let checkout = () => {
+        if (ward && district && city && paymentMethod) {
+
+        } else {
+            message.warning("Please fill in all information");
+        }
+    }
+
     return (
         <div style={{ padding: "200px", width: "60%", float: "left" }}>
-            <Space size={10} style={{ width: "100%" }}>
-                {cityList &&
-                <Select placeholder="Select city" style={{width: "200px"}} onChange={getDistrictList}>
-                    { cityList.map((element, index) => { return <Select.Option value={element.ProvinceID} key={index}>{element.ProvinceName}</Select.Option> }) }
-                </Select>
-                }
-                {districtList && 
-                <Select placeholder="Select district" style={{width: "200px"}} onChange={getWardList}>
-                    { districtList.map((element, index) => { return <Select.Option value={element.DistrictID} key={index}>{element.DistrictName}</Select.Option> }) }
-                </Select>
-                }
-                {wardList && 
-                <Select placeholder="Select ward" style={{width: "200px"}} onChange={getExpArr}>
-                    { wardList.map((element, index) => { return <Select.Option value={element.WardCode} key={index}>{element.WardName}</Select.Option> }) }
-                </Select>
-                }
-            </Space>
-            <Space style={{ width: "100%", paddingBottom: "10px", paddingLeft: "10px" }}>
+            <Space direction="vertical">
+                <Space size={10} style={{ width: "100%" }}>
+                    {cityList &&
+                    <Select placeholder="Select city" style={{width: "200px"}} onChange={getDistrictList}>
+                        { cityList.map((element, index) => { return <Select.Option value={element.ProvinceID} key={index}>{element.ProvinceName}</Select.Option> }) }
+                    </Select>
+                    }
+                    {districtList && 
+                    <Select placeholder="Select district" style={{width: "200px"}} onChange={getWardList}>
+                        { districtList.map((element, index) => { return <Select.Option value={element.DistrictID} key={index}>{element.DistrictName}</Select.Option> }) }
+                    </Select>
+                    }
+                    {wardList && 
+                    <Select placeholder="Select ward" style={{width: "200px"}} onChange={getExpArr}>
+                        { wardList.map((element, index) => { return <Select.Option value={element.WardCode} key={index}>{element.WardName}</Select.Option> }) }
+                    </Select>
+                    }
+                </Space>
                 <p>{expectedArrival}</p>
+                <Select placeholder="Payment method" style={{ width: "620px" }} onChange={(value) => setPaymentMethod(value)}>
+                    <Select.Option value={"cod"}>Cash On Delivery</Select.Option>
+                    <Select.Option value={"credit"}>Credit Card</Select.Option>
+                </Select>
+                <Button type="primary" style={{ marginLeft: "auto" }} onClick={checkout}>Checkout</Button>
             </Space>
-            <Select placeholder="Payment method" style={{ width: "620px" }} onChange={(value) => setPaymentMethod(value)}>
-                <Select.Option value={"cod"}>Cash On Delivery</Select.Option>
-                <Select.Option value={"credit"}>Credit Card</Select.Option>
-            </Select>
         </div>
     )
 
